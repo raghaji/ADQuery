3726ede301faa6fa3b8d7d2778b2f555400e07cf
Administrator@GOOGLE.LOCAL <Administrator@GOOGLE.LOCAL>
2021-02-01 22:07:19 +0530

    Adding all files

---
+++ Untitled2.ps1
@@ -0,0 +1,56 @@
+﻿<# 
+Ad-Hoc remoting in powershell
+Get-WmiObject -Class win32_computersystem -ComputerName 192.168.1.2 -Credential administrator@google.local
+#>
+
+<#
+Interactive remoting
+
+Enter-PSSession -ComputerName 192.168.1.2 -Credential administrator@google.local
+#>
+
+<#Implicit remoting
+
+$remotepc = New-PSSession -ComputerName 192.168.1.2 -Credential $Credential
+
+Invoke-Command -Session $remotepc -ScriptBlock { Import-Module ActiveDirectory }
+
+###Import-PSSession -Session $remotepc -Prefix Remote -AllowClobber -Module ActiveDirectory
+
+Export-PSSession -Session $remotepc -CommandName *ad* -OutputModule RemoteModuleAD4 -AllowClobber
+
+Import-Module RemoteModuleAD4
+$remotepc|Remove-PSSession
+
+#>
+
+<#
+Persistent session
+
+$remotepc = New-PSSession -ComputerName 192.168.1.2 -Credential $Credential
+Invoke-Command -Session $remotepc -ScriptBlock {Get-WmiObject -Class CIM_ComputerSystem}
+
+#>
+
+
+
+<#
+Get password as secure string
+
+$password = Read-Host -AsSecureString
+Write-Host $password
+
+#>
+
+<# 
+How to store string as username and password and used in command
+
+$User = "Google\administrator"
+$PWord = Read-Host -AsSecureString
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+
+Get-WmiObject -Class win32_computersystem -ComputerName 192.168.1.2 -Credential $Credential
+
+#>
+
+
---
+++ Untitled6.ps1
@@ -0,0 +1,2 @@
+﻿Write-Host "           "(($item.Trim("CN=")).substring(0,($item.Trim("CN=")).IndexOf(',')))
+($ccommand |select ManagedBy).ManagedBy
