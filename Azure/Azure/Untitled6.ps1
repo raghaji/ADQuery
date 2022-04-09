b4e70546b6ef76d168b58f9813bc312b630e014d
Raghaji Raul <raulraghaji@gmail.com>
2022-04-09 16:09:30 +0530

    Deleted files

--- Azure/Azure/Untitled6.ps1
+++
@@ -1,2 +0,0 @@
-﻿Write-Host "           "(($item.Trim("CN=")).substring(0,($item.Trim("CN=")).IndexOf(',')))
-($ccommand |select ManagedBy).ManagedBy
--- Azure/Untitled5.ps1
+++
@@ -1,4 +0,0 @@
-﻿$TargetName = “INVS”
-$RC = Get-WinEvent -Computer $TargetName -FilterHashtable @{ Logname = ‘Security’; ID = 4672 } -MaxEvents 1 | Select @{ N = ‘User’; E = { $_.Properties[1].Value } }, TimeCreated
-$RC.Username
-$RC.TimeCreated
